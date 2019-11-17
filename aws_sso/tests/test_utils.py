c7d80b7ca0ed2479a3e47223c783653d46310fcf
Rajan Khullar <rkhullar03@gmail.com>
2019-11-17 14:42:47 -0500

    save

--- aws_sso/tests/test_utils.py
+++ aws_sso/tests/test_utils.py
@@ -1,19 +1,39 @@
-from ..utils import *
+from parameterized import parameterized
+from nose.tools import assert_equal
 from unittest import TestCase
+from ..utils import *
 
 
 class UtilsTest(TestCase):
 
     def test_package_root(self):
-        self.assertEquals(get_package_root(), 'aws_sso')
+        assert_equal(get_package_root(), 'aws_sso')
+
+    @parameterized.expand([
+        ('localhost', None),
+        ('example.com', 'example.com'),
+        ('sso.example.com', 'example.com')
+    ])
+    def test_infer_domain(self, value, expected):
+        actual = infer_domain(value)
+        assert_equal(expected, actual)
+
+    @parameterized.expand([
+        ('localhost', 'test_user', 'test_user'),
+        ('example.com', 'test_user', 'example.com\\test_user'),
+        ('sso.example.com', 'test_user', 'example.com\\test_user')
+    ])
+    def test_build_domain_username(self, domain, username, expected):
+        actual = build_domain_username(domain, username)
+        assert_equal(expected, actual)
 
 
 class HelloTest(TestCase):
 
     def test_stdout(self):
-        message: str = 'hello world'
+        message: str = 'this is the kitchen'
         with mocked_stdio() as context:
             hello(message=message, count=1)
         context.stdout.seek(0)
         actual = context.stdout.read().strip('\r\n')
-        self.assertEqual(message, actual)
+        assert_equal(message, actual)
--- aws_sso/utils/__init__.py
+++ aws_sso/utils/__init__.py
@@ -1,3 +1,3 @@
-from .hello import hello
+from .hello_world import hello
 from .mocked_stdio import mocked_stdio
 from .misc import *
