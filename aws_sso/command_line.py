9b3b715f0db4dd82e8a8bca3e50923c29ce09b5a
Rajan Khullar <rkhullar03@gmail.com>
2019-11-17 23:21:38 -0500

    adding saml loader and discovery

--- aws_sso/command_line.py
+++ aws_sso/command_line.py
@@ -1,10 +1,18 @@
+from typing import Dict, Optional, NamedTuple, Iterator
+from requests_ntlm import HttpNtlmAuth
 from argparse import ArgumentParser
+import xml.etree.ElementTree as ET
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 from pathlib import Path
 from .utils import *
 
 import configparser
+import requests
 import keyring
 import getpass
+import base64
+import re
 
 
 def build_parser() -> ArgumentParser:
@@ -21,9 +29,9 @@ def build_parser() -> ArgumentParser:
     add_site_parser.add_argument('--username', '-u', type=str, required=True)
     add_site_parser.add_argument('--password', '-p', type=str, required=False)
 
-    profile_parser = subparsers.add_parser('profile')
-    profile_parser.add_argument('--domain', '-d', type=str, required=True)
-    profile_parser.add_argument('--skip-names', action='count')
+    discover_parser = subparsers.add_parser('discover')
+    discover_parser.add_argument('--domain', '-d', type=str, required=True)
+    discover_parser.add_argument('--skip-names', action='count')
 
     return parser
 
@@ -48,6 +56,76 @@ def add_site(domain: str, username: str, password: str):
         config.write(f)
 
 
+def fetch_saml_token(domain: str, username: str, password: str, verify_ssl: bool = True) -> Optional[str]:
+    idp_entry_url: str = f'https://{domain}/adfs/ls/IdpInitiatedSignOn.aspx?loginToRp=urn:amazon:webservices'
+    domain_username: str = build_domain_username(domain, username)
+    session = requests.Session()
+    session.auth = HttpNtlmAuth(domain_username, password)
+    response = session.get(idp_entry_url, verify=verify_ssl)
+    idp_submit_url: str = response.url
+    payload: Dict[str, str] = dict()
+    soup = BeautifulSoup(response.text, features='html5lib')
+    for input_tag in soup.find_all(re.compile('input')):
+        name, value = input_tag.get('name'), input_tag.get('value', '')
+        if string_contains(text=name.lower(), keys=['user', 'email']):
+            payload[name] = domain_username
+        elif string_contains(text=name.lower(), keys=['pass']):
+            payload[name] = password
+        else:
+            payload[name] = value
+    for tag in soup.find_all('form'):
+        action, login_id = tag.get('action'), tag.get('id')
+        if action and login_id == 'loginForm':
+            parsed_url = urlparse(idp_entry_url)
+            idp_submit_url = f'{parsed_url.scheme}://{parsed_url.netloc}{action}'
+    response = session.post(idp_submit_url, data=payload, verify=verify_ssl)
+    session.close()
+    soup = BeautifulSoup(response.text, features='html5lib')
+    for input_tag in soup.find_all(re.compile('input')):
+        if input_tag.get('name') == 'SAMLResponse':
+            return input_tag.get('value')
+
+
+class RoleTuple(NamedTuple):
+    principle_arn: str
+    role_arn: str
+
+    @staticmethod
+    def from_saml(saml: str) -> 'RoleTuple':
+        principle, role = saml.split(',')
+        if 'saml-provider' in role:
+            principle, role = role, principle
+        return RoleTuple(principle, role)
+
+
+def read_roles_from_saml(assertion: str) -> Iterator[RoleTuple]:
+    root = ET.fromstring(base64.b64decode(assertion))
+    xml_attr = '{urn:oasis:names:tc:SAML:2.0:assertion}Attribute'
+    url_role = 'https://aws.amazon.com/SAML/Attributes/Role'
+    xml_attr_val = '{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue'
+    return (RoleTuple.from_saml(value.text) for attr in root.iter(xml_attr) if attr.get('Name') == url_role for value in attr.iter(xml_attr_val))
+
+
+def discover(domain: str, skip_names: bool = False):
+    service_name: str = get_package_root()
+    config_dir = Path('~/.config').expanduser() / service_name
+    sites_path = config_dir / 'sites.ini'
+    config = configparser.ConfigParser()
+    if sites_path.exists():
+        with sites_path.open('r') as f:
+            config.read_file(f)
+    if domain not in config.sections():
+        raise EnvironmentError(dict(message='site not added', domain=domain))
+    username: str = config[domain]['username']
+    domain_username: str = build_domain_username(domain, username)
+    password: str = keyring.get_password(service_name=service_name, username=domain_username)
+    assertion = fetch_saml_token(domain, username, password)
+    if not assertion:
+        raise ValueError(dict(message='invalid credentials', domain=domain, username=username))
+    for role_tuple in read_roles_from_saml(assertion):
+        print(role_tuple)
+
+
 def main():
     parser = build_parser()
     args = parser.parse_args()
@@ -59,3 +137,5 @@ def main():
         args.password = args.password or getpass.getpass(prompt='password: ', stream=None)
         add_site(domain=args.domain, username=args.username, password=args.password)
 
+    elif args.action == 'discover':
+        discover(domain=args.domain, skip_names=bool(args.skip_names))
--- aws_sso/utils/misc.py
+++ aws_sso/utils/misc.py
@@ -17,3 +17,10 @@ def build_domain_username(domain: str, username: str) -> str:
     domain: str = infer_domain(domain)
     parts: List[str] = [domain or '', username]
     return delimiter.join(parts).strip(delimiter)
+
+
+def string_contains(text: str, keys: List[str]) -> bool:
+    return any(key in text for key in keys)
+
+
+__all__ = ['get_package_root', 'infer_domain', 'build_domain_username', 'string_contains']
