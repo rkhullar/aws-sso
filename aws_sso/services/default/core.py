1b2d6578abbcdf3225e418d6f963c9ceb95f4ce8
Rajan Khullar <rkhullar03@gmail.com>
2019-11-30 11:53:44 -0500

    working on adding sites

--- aws_sso/services/default/core.py
+++ aws_sso/services/default/core.py
@@ -1,23 +1,3 @@
-def add_site(domain: str, username: str, password: str):
-    service_name: str = get_package_root()
-    domain_username: str = build_domain_username(domain, username)
-    keyring.set_password(service_name=service_name, username=domain_username, password=password)
-
-    config_dir = Path('~/.config').expanduser() / service_name
-    config_dir.mkdir(parents=True, exist_ok=True)
-
-    sites_path = config_dir / 'sites.ini'
-    config = configparser.ConfigParser()
-    if sites_path.exists():
-        with sites_path.open('r') as f:
-            config.read_file(f)
-    if domain not in config.sections():
-        config.add_section(domain)
-    config[domain]['username'] = username
-    with sites_path.open('w') as f:
-        config.write(f)
-
-
 def discover(domain: str, skip_names: bool = False):
     service_name: str = get_package_root()
     config_dir = Path('~/.config').expanduser() / service_name
--- aws_sso/services/sites/worker.py
+++ aws_sso/services/sites/worker.py
@@ -1,11 +1,35 @@
 from ...abstract import AbstractService
-from ...utils import register_action
+from ...utils import build_domain_username, get_package_root, register_action
+from pathlib import Path
+
+import configparser
 import getpass
+import keyring
 
 
 class SiteWorker(AbstractService):
 
+    @staticmethod
+    def add_site(domain: str, username: str, password: str):
+        service_name: str = get_package_root()
+        domain_username: str = build_domain_username(domain, username)
+        keyring.set_password(service_name=service_name, username=domain_username, password=password)
+
+        config_dir = Path('~/.config').expanduser() / service_name
+        config_dir.mkdir(parents=True, exist_ok=True)
+
+        sites_path = config_dir / 'sites.ini'
+        config = configparser.ConfigParser()
+        if sites_path.exists():
+            with sites_path.open('r') as f:
+                config.read_file(f)
+        if domain not in config.sections():
+            config.add_section(domain)
+        config[domain]['username'] = username
+        with sites_path.open('w') as f:
+            config.write(f)
+
     @register_action('add')
     def handle_add(self):
         password = self.params.password or getpass.getpass(prompt='password: ', stream=None)
-        # add_site(domain=self.params.domain, username=self.params.username, password=password)
+        self.add_site(domain=self.params.domain, username=self.params.username, password=password)
