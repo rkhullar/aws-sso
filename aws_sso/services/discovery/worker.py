267aa5682e8ec0662e4ec21aa2bed128a98019c4
Rajan Khullar <rkhullar03@gmail.com>
2019-12-15 11:12:07 -0500

    able to read saml roles

--- aws_sso/services/discovery/worker.py
+++ aws_sso/services/discovery/worker.py
@@ -2,6 +2,7 @@ from ...abstract import AbstractService
 from ...model import CommonParams
 from ..sites import *
 from ...utils import register_action
+from .core import part_1, part_2
 
 
 class DiscoveryWorker(AbstractService):
@@ -13,7 +14,11 @@ class DiscoveryWorker(AbstractService):
         self.tools['sites'] = self.build_site_worker()
 
     def discover(self, domain: str, skip_names: bool = True):
-        print(self.tools['sites'].get_site_login(domain))
+        site_login: SiteLogin = self.tools['sites'].get_site_login(domain)
+        idp_submit_url, payload = part_1(domain=site_login.domain, username=site_login.username, password=site_login.password)
+        role_tuples = part_2(idp_submit_url, payload)
+        for role_tuple in role_tuples:
+            print(role_tuple)
 
     @register_action('default')
     def main(self):
